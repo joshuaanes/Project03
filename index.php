8897b9df6ebe9c5ebc9776dabe0712cd9a21d466
joshuaanes <joshua.anes.aepi@gmail.com>
2016-12-19 21:55:24 -0500

    index of website, the long scrolly page

---
+++ index.php
@@ -0,0 +1,79 @@
+   <?php include "inc/top.inc"; ?>
+   <script src="http://code.jquery.com/jquery.js"></script>
+   <script src="js/menu-highlighter.js"></script>
+  
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+    <link href="che.css" rel="stylesheet">
+
+  
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-square" src="images/fam.jpg" alt="Generic placeholder image" width="400" height="400">
+          <h2></h2>
+          <p>Joshua Anes is a senior at the University of Rochester. Joshua is pursuing a degree in Political Science and a minor in Jewish Studies.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+       <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="images/kirtzman.png" alt="Generic placeholder image">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Kirtzman Strategies<span class="text-muted"></span></h2>
+          <p class="lead">This past summer, I had the unique opportunity to intern at an exciting new strategic communincations and strategies firm, Kirtzman Strategies. For Kirtzman, I authored press releases, built media lists, and developed a marketing deck, among many accomplishments.</p>
+        </div>
+       
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+       <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="images/gsg.jpg" alt="Generic placeholder image" height="200" width="200">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Global Strategy Group<span class="text-muted"></span></h2>
+          <p class="lead">I had the opportunity to intern at Global Strategy Group following my sophomore year. For GSG, I prepared written materials for advocacy campaigns, such as draft letters and press releases, monitored events in the news for clients and staff, and performed background research for clients and potential new business, among many accomplishments.
+ </p>
+        </div>
+       
+      </div>
+
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+      <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="images/pallone.jpg" alt="Generic placeholder image" height="200" width="200">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Office of Congressman Frank Pallone, Jr.<span class="text-muted"></span></h2>
+          <p class="lead">After my freshmen year I interned in the Washington, D.C., Office of Congressman Frank Pallone, Jr. For the Congressman, I fielded constituent calls and effectively managed correspondence during the height of the immigration debate and analyzed legislation, prepared memoranda, and worked directly with staffers and legislative assistants to help them make recommendations to the Congressman on pending bills. </p>
+        </div>
+        
+      </div>
+
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+ <?php include "inc/footer.inc"; ?>
