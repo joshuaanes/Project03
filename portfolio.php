cf8296ab99bae682b35ab671205fdb573161be42
joshuaanes <joshua.anes.aepi@gmail.com>
2016-12-19 21:56:09 -0500

    portfolio page for website

---
+++ portfolio.php
@@ -0,0 +1,30 @@
+<?php include "inc/top.inc"; ?>
+<link href="css/portfolio.css" rel="stylesheet">
+ <script src="http://code.jquery.com/jquery.js"></script>
+ <script src="js/menu-highlighter.js"></script>
+ <h1>Portfolio</h1>
+ <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-square" src="images/labs.gif" alt="Generic placeholder image" width="140" height="140">
+          <h2>Lab 1</h2>
+          <p>This assignment was to re-familiarize myself with the process of using a hosted web service to setup my own web space and publish a simple webpage on the Web. </p>
+          <p><a class="btn btn-default" href="http://urcsc170.org/janes/lab01/" role="button">Check out the site &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-square" src="images/labs.gif" alt="Generic placeholder image" width="140" height="140">
+          <h2>Lab 6</h2>
+          <p>This assignment was to create a webpage using proper CSS architecture to keep the original intent of the design of the original CSS. This was a successful lab.</p>
+          <p><a class="btn btn-default" href="http://urcsc170.org/janes/lab6-starter-files/" role="button">Check out the site &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-square" src="images/labs.gif" alt="Generic placeholder image" width="140" height="140">
+          <h2>Lab 7</h2>
+          <p>The purpose of this lab assignment was to select and install new web fonts for the starter files provided to improve the design of the site. This was also a successful lab.</p>
+          <p><a class="btn btn-default" href="http://urcsc170.org/janes/lab07-starter-files/" role="button">Check out the site &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+     <?php include "inc/footer.inc"; ?>
